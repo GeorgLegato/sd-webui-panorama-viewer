59902c0f50195c7517ab7665690ae2715de2d17a
GeorgLegato <GeorgLegato@online.de>
2023-03-05 17:09:26 +0100

    starts, todo: fix url/equiadapter

---
+++ javascript/panoramaviewer-ext.js
@@ -0,0 +1,88 @@
+const openpanorama = {
+	frame: null
+};
+
+function panorama_send_image(dataURL, name = "Embed Resource") {
+	openpanorama.frame.contentWindow.postMessage({
+		type: "panoramaviewer/set-panorama",
+		image: {
+			dataURL,
+			resourceName: name,
+		},
+	});
+}
+
+function panorama_gototab(tabname = "Panorama-3D-Viewer", tabsId = "tabs") {
+	Array.from(
+		gradioApp().querySelectorAll(`#${tabsId} > div:first-child button`)
+	).forEach((button) => {
+		if (button.textContent.trim() === tabname) {
+			button.click();
+		}
+	});
+}
+
+
+async function panorama_get_image_from_gallery() {
+	var buttons = gradioApp().querySelectorAll(
+		'[style="display: block;"].tabitem div[id$=_gallery] .gallery-item'
+	);
+	var button = gradioApp().querySelector(
+		'[style="display: block;"].tabitem div[id$=_gallery] .gallery-item.\\!ring-2'
+	);
+
+	if (!button) button = buttons[0];
+
+	if (!button)
+		throw new Error("[panorama_viewer] No image available in the gallery");
+
+/* only use file url, not data url 
+
+const canvas = document.createElement("canvas");
+	const image = document.createElement("img");
+	image.src = button.querySelector("img").src;
+
+
+	await image.decode();
+
+	canvas.width = image.width;
+	canvas.height = image.height;
+
+	canvas.getContext("2d").drawImage(image, 0, 0);
+
+	return canvas.toDataURL();
+	*/
+	return button.querySelector("img").src
+}
+
+function panorama_send_gallery(name = "Embed Resource") {
+    console.log("hello p360")
+    panorama_get_image_from_gallery()
+		.then((dataURL) => {
+			// Send to panorama-viewer
+			panorama_send_image(dataURL, name);
+
+			// Change Tab
+			panorama_gototab();
+		})
+		.catch((error) => {
+			console.warn("[panoramaviewer] No image selected to send to panorama viewer");
+		});
+}
+
+
+function openpanoramajs() {
+	const frame = gradioApp().getElementById("panoviewer-iframe");
+	openpanorama.frame = frame;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	const onload = () => {
+		if (gradioApp().getElementById("panoviewer-iframe")) {
+			openpanoramajs();
+		} else {
+			setTimeout(onload, 10);
+		}
+	};
+	onload();
+});
--- scripts/panorama-3dviewer.py
+++ scripts/panorama-3dviewer.py
@@ -1,136 +1,21 @@
+from modules import script_callbacks, scripts, shared
+import os
 import html
-
-from ldm.modules.encoders.modules import FrozenCLIPEmbedder
 from modules import script_callbacks, shared
-
 import gradio as gr
 
-
-css = """
-.tokenizer-token{
-    cursor: pointer;
-}
-.tokenizer-token-0 {background: rgba(255, 0, 0, 0.05);}
-.tokenizer-token-0:hover {background: rgba(255, 0, 0, 0.15);}
-.tokenizer-token-1 {background: rgba(0, 255, 0, 0.05);}
-.tokenizer-token-1:hover {background: rgba(0, 255, 0, 0.15);}
-.tokenizer-token-2 {background: rgba(0, 0, 255, 0.05);}
-.tokenizer-token-2:hover {background: rgba(0, 0, 255, 0.15);}
-.tokenizer-token-3 {background: rgba(255, 156, 0, 0.05);}
-.tokenizer-token-3:hover {background: rgba(255, 156, 0, 0.15);}
-"""
-
-
-def tokenize(text, input_is_ids=False):
-    clip: FrozenCLIPEmbedder = shared.sd_model.cond_stage_model.wrapped
-
-    if input_is_ids:
-        tokens = [int(x.strip()) for x in text.split(",")]
-    else:
-        tokens = clip.tokenizer(text, truncation=False, add_special_tokens=False)["input_ids"]
-
-    vocab = {v: k for k, v in clip.tokenizer.get_vocab().items()}
-
-    code = ''
-    ids = []
-
-    current_ids = []
-    class_index = 0
-
-    def dump(last=False):
-        nonlocal code, ids, current_ids
-
-        words = [vocab.get(x, "") for x in current_ids]
-
-        def wordscode(ids, word):
-            nonlocal class_index
-            res = f"""<span class='tokenizer-token tokenizer-token-{class_index%4}' title='{html.escape(", ".join([str(x) for x in ids]))}'>{html.escape(word)}</span>"""
-            class_index += 1
-            return res
-
-        try:
-            word = bytearray([clip.tokenizer.byte_decoder[x] for x in ''.join(words)]).decode("utf-8")
-        except UnicodeDecodeError:
-            if last:
-                word = "❌" * len(current_ids)
-            elif len(current_ids) > 4:
-                id = current_ids[0]
-                ids += [id]
-                local_ids = current_ids[1:]
-                code += wordscode([id], "❌")
-
-                current_ids = []
-                for id in local_ids:
-                    current_ids.append(id)
-                    dump()
-
-                return
-            else:
-                return
-
-        word = word.replace("</w>", " ")
-
-        code += wordscode(current_ids, word)
-        ids += current_ids
-
-        current_ids = []
-
-    for token in tokens:
-        token = int(token)
-        current_ids.append(token)
-
-        dump()
-
-    dump(last=True)
-
-    ids_html = f"""
-<p>
-Token count: {len(ids)}<br>
-{", ".join([str(x) for x in ids])}
-</p>
-"""
-
-    return code, ids_html
-
+usefulDirs = scripts.basedir().split(os.sep)[-2:]
 
 def add_tab():
-    with gr.Blocks(analytics_enabled=False, css=css) as ui:
-        gr.HTML(f"""
-<style>{css}</style>
-<p>
-Before your text is sent to the neural network, it gets turned into numbers in a process called tokenization. These tokens are how the neural network reads and interprets text. Thanks to our great friends at Shousetsu愛 for inspiration for this feature.
-</p>
-""")
+    with gr.Blocks(analytics_enabled=False) as ui:
+        gr.HTML(value=f"<iframe id=\"panoviewer-iframe\" class=\"border-2 border-gray-200\" src=\"file={usefulDirs[0]}/{usefulDirs[1]}/scripts/viewer.html\" title='description'></iframe>")
+    return [(ui, "Panorama-3D-Viewer", "panorama-3dviewer")]
 
-        with gr.Tabs() as tabs:
-            with gr.Tab("Text input", id="input_text"):
-                prompt = gr.Textbox(label="Prompt", elem_id="tokenizer_prompt", show_label=False, lines=8, placeholder="Prompt for tokenization")
-                go = gr.Button(value="Tokenize", variant="primary")
-
-            with gr.Tab("ID input", id="input_ids"):
-                prompt_ids = gr.Textbox(label="Prompt", elem_id="tokenizer_prompt", show_label=False, lines=8, placeholder="Ids for tokenization (example: 9061, 631, 736)")
-                go_ids = gr.Button(value="Tokenize", variant="primary")
-
-        with gr.Tabs():
-            with gr.Tab("Text"):
-                tokenized_text = gr.HTML(elem_id="tokenized_text")
-
-            with gr.Tab("Tokens"):
-                tokens = gr.HTML(elem_id="tokenized_tokens")
-
-        go.click(
-            fn=tokenize,
-            inputs=[prompt],
-            outputs=[tokenized_text, tokens],
-        )
-
-        go_ids.click(
-            fn=lambda x: tokenize(x, input_is_ids=True),
-            inputs=[prompt_ids],
-            outputs=[tokenized_text, tokens],
-        )
-
-    return [(ui, "Panorama-3dviewer", "panorama-3dviewer")]
-
+def after_component(component, **kwargs):
+    # Add button to both txt2img and img2img tabs
+    if kwargs.get("elem_id") == "extras_tab":
+        basic_send_button = gr.Button("Send to PanoramaViewer", elem_id=f"sendto_panorama_button")
+        basic_send_button.click(None, [], None, _js="() => panorama_send_gallery('WebUI Resource')")
 
 script_callbacks.on_ui_tabs(add_tab)
+script_callbacks.on_after_component(after_component)
