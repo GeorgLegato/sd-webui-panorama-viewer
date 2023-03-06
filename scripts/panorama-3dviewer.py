6bdd44168e33650eb8e82f6d54d51665503f9da8
GeorgLegato <GeorgLegato@online.de>
2023-03-06 01:34:31 +0100

    changed Tab name to "panorama viewer", w/o 3d

--- scripts/panorama-3dviewer.py
+++ scripts/panorama-3dviewer.py
@@ -9,7 +9,7 @@ usefulDirs = scripts.basedir().split(os.sep)[-2:]
 def add_tab():
     with gr.Blocks(analytics_enabled=False) as ui:
         gr.HTML(value=f"<iframe id=\"panoviewer-iframe\" class=\"border-2 border-gray-200\" src=\"file={usefulDirs[0]}/{usefulDirs[1]}/scripts/viewer.html\" title='description'></iframe>")
-    return [(ui, "Panorama-3D-Viewer", "panorama-3dviewer")]
+    return [(ui, "Panorama Viewer", "panorama-3dviewer")]
 
 def after_component(component, **kwargs):
     # Add button to both txt2img and img2img tabs
